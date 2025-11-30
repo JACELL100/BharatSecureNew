70597d6c11e7280473792801105058012f23dcf2
code-runner1306 <mayankhmehtaw@gmail.com>
2025-12-01 02:40:16 +0530

    updated origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173", 
-    "https://yourdomain.com", 
+    "https://bharat-secure-ochre.vercel.app/", 
 ]
 
 # Application definition
